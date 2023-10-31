80789f375968e7edb1bfcddec883e3f27b975afa
girliehappy <etetimh@gmail.com>
2023-10-31 07:57:18 -0700

    Task 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle:
     print_symbol = "#"
     
     def __init__(self, width=0, height=0):
-         """Initialize a new Rectangle.
+        """Initialize a new Rectangle.
 
         Args:
             width (int): The width of the new Rectangle.
@@ -58,33 +58,32 @@ class Rectangle:
 
     def __str__(self):
         """Print the rectangle with the character #.
-        If width or height is equal to 0.
-        Return an empty string.
+        If width or height is equal to 0, return an empty string.
         """
         if self.width == 0 or self.height == 0:
             return ""
         rectangle_str = ""
         for _ in range(self.height):
             rectangle_str += "#" * self.width + "\n"
-        return rectangle_str[:-1] """This line removes trailing newline."""
+        return rectangle_str[:-1]
 
     def __repr__(self):
         """Returns a string representation of the Rectangle."""
         return f"Rectangle({self.width}, {self.height})"
 
     def __del__(self):
-        """Deletion of instance of Rectangle."""
+        """Deletion of an instance of Rectangle."""
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Returns the biggest rectangle based on the area."""
-        if not isinstance(rect_1, Rectangle);
+        if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
             return rect_1
-        else"
-        return rect_2
+        else:
+            return rect_2
